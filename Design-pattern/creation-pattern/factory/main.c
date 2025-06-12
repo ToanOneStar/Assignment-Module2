5c0e437d9a96ca7cf4437f5240d95f5096612cc0
ToanOneStar <congtoanfan@gmail.com>
2025-06-12 22:40:04 +0700

    Design-pattern

--- Design-pattern/creation-pattern/factory/main.c
+++ Design-pattern/creation-pattern/factory/main.c
@@ -6,31 +6,35 @@
 #include "pressure/pressure_sensor.c"
 #include "temperature/temperature-sensor.c"
 
-int main() {
-
-    Sensor* tempSensor = createSensor(SENSOR_TEMPERATURE);
-    if (tempSensor != NULL) {
+int main()
+{
+    Sensor *tempSensor = createSensor(SENSOR_TEMPERATURE);
+    if (tempSensor != NULL)
+    {
         tempSensor->init();
         printf("Temperature: %.2f°C\n", tempSensor->readData());
         free(tempSensor);
     }
 
-    Sensor* humiditySensor = createSensor(SENSOR_HUMIDITY);
-    if (humiditySensor != NULL) {
+    Sensor *humiditySensor = createSensor(SENSOR_HUMIDITY);
+    if (humiditySensor != NULL)
+    {
         humiditySensor->init();
         printf("Humidity: %.2f%%\n", humiditySensor->readData());
         free(humiditySensor);
     }
 
-    Sensor* lightSensor = createSensor(SENSOR_LIGHT);
-    if (lightSensor != NULL) {
+    Sensor *lightSensor = createSensor(SENSOR_LIGHT);
+    if (lightSensor != NULL)
+    {
         lightSensor->init();
         printf("Light Intensity: %.2f lux\n", lightSensor->readData());
         free(lightSensor);
     }
 
-    Sensor* pressureSensor = createSensor(SENSOR_PRESSURE);
-    if (pressureSensor != NULL) {
+    Sensor *pressureSensor = createSensor(SENSOR_PRESSURE);
+    if (pressureSensor != NULL)
+    {
         pressureSensor->init();
         printf("Pressure: %.2f atm\n", pressureSensor->readData());
         free(pressureSensor);
